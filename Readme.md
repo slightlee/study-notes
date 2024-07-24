a4006a2752c3ce99f6050cb08965d75df324f373
demain_lee <limingwei@mentpeak.com>
2024-07-24 16:18:47 +0800

    使用nextra构建docs

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+## 该文档使用nextra框架构建。
+
+## 参考：
+
+    nextra官方文档： https://nextra.site/
+
+    nextra中文文档： https://www.nextracn.com/
---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+export default {
+    logo: <span>一点笔记</span>,
+    project: {
+      link: 'https://github.com/shuding/nextra'
+    },
+    logo: <b>一点笔记</b>
+    // ... other theme options
+  }
+//   https://nextra.site/docs/docs-theme/theme-configuration
